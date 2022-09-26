d86f97409cf42cb295b2969104bc421ed6eca4ca
Maria Macarena Gomez Pujal <mmgomezpujal@gmail.com>
2022-09-26 08:32:51 -0300

    Quinta Versión c/ ckeditor

--- blog_coder/BlogApp/models.py
+++ blog_coder/BlogApp/models.py
@@ -1,5 +1,7 @@
 from distutils.command.upload import upload
+from tokenize import blank_re
 from django.db import models
+from ckeditor.fields import RichTextField
 
 class Articulo(models.Model): 
     titulo = models.CharField(max_length=128)
@@ -7,7 +9,11 @@ class Articulo(models.Model):
     fecha = models.DateField()
     autor = models.CharField(max_length=128)
     email_autor = models.EmailField()
-    cuerpo = models.TextField(null=True, blank=True)
-    imagen = models.ImageField(upload_to='imagenes', null=True, blank=True)
+    cuerpo = RichTextField(blank=True, null= True)
+    # imagen = models.ImageField(upload_to='imagenes', null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.titulo}'
+
 
 # Create your models here.
